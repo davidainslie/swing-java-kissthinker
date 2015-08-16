ab9271f95eaae03a73e66d7ba6627ce6766c7b49
David Ainslie <dainslie@gmail.com>
2015-08-16 17:33:45 +0100

    Depend on Java KISS Thinker

---
+++ src/test/java/blah/BlahTest.java
@@ -0,0 +1,13 @@
+package blah;
+
+import org.junit.Test;
+import com.kissthinker.collection.map.MapUtil;
+
+public class BlahTest
+{
+    @Test
+    public void testName() throws Exception
+    {
+        System.out.println(MapUtil.hashMap());
+    }
+}
